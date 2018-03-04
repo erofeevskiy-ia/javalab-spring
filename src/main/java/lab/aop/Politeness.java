a852aaa1c7b5393917bd16a49a215637ff66019e
Igor <igorer27@yandex.ru>
2018-03-04 23:44:45 +0300

    Tests successfully completed

--- src/main/java/lab/aop/Politeness.java
+++ src/main/java/lab/aop/Politeness.java
@@ -1,9 +1,9 @@
 package lab.aop;
 
 import lab.model.Person;
+import lab.model.Squishee;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import lab.model.Squishee;
 
 @Aspect
 public class Politeness {
@@ -13,20 +13,27 @@ public class Politeness {
         System.out.printf("Hello %s. How are you doing?%n", person.getName());
     }
 
+    @Pointcut("execution(* sellSquishee(..))")
+    public void sellSquishee() {
+    }
+
     @AfterReturning(pointcut = "execution(* sellSquishee(..))",
             returning = "retVal", argNames = "retVal")
     public void askOpinion(Squishee retVal) {
         System.out.printf("Is %s Good Enough?%n", retVal.getName());
     }
 
+    @AfterThrowing("sellSquishee()")
     public void sayYouAreNotAllowed() {
         System.out.println("Hmmm... \n");
     }
 
+    @After("sellSquishee()")
     public void sayGoodBye() {
         System.out.println("Good Bye! \n");
     }
 
+    @Around("sellSquishee()")
     public Object sayPoliteWordsAndSell(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("Hi! \n");
         Object retVal = pjp.proceed();
